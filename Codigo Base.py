22fc0cc196a815bd20ee0af1ae87dfa662cdf70b
Olivares Meneses | TEC de Monterrey <A00843697@tec.mx>
2025-04-02 19:30:39 -0600

    Cambios Realizados Intento de Corrección de impresión de matriz

--- Codigo Base.py
+++ Codigo Base.py
@@ -14,12 +14,13 @@ def Imprimir_Tablero():
     Filas = ["A", "B", "C", "D", "E"]
     print("\033[1m\t1\t2\t3\t4\t5\033[0m")
     print("    "+"+-------"*5 + "+")
-    for n in range(len(matrix)):    
-        print("\033[1m"+str(Filas[n]) + "\033[0m" + "   |   "+str(matrix[n][n])*len(matrix) + "   |")
+    for n in range(len(matrix)):
+        print("\033[1m"+str(Filas[n]) + "\033[0m" + ("   |   "+str(matrix[n][n]))*len(matrix) + "   |")
         print("    "+"+-------"*5 + "+")
-
+    
 def Eliminar_Ficha(m, n): #[n, m]
     matrix[m, n] = "."
-print(matrix)
 Inicializar_Tablero()
+matrix[2][2] = "."
+print(matrix)
 Imprimir_Tablero()
